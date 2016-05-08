0f552c8b71e4b35dfa185f899cf3429306f74505
Bishal Napit <mebishalnapit@gmail.com>
2016-05-08 19:44:19 +0545

    Escaping fix done in searchform.php file

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
  * @package Creative Blog
  */
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
     <label>
         <span class="screen-reader-text"><?php echo esc_html_e('Search for:', 'masonry-brick') ?></span>
         <input type="search" class="search-field" placeholder="<?php esc_html_e('Search for&hellip;', 'masonry-brick') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_html__('Search for:', 'masonry-brick') ?>" />
